952252cc79ed3a5b84ce8476a4a97d2212aaa261
Dipin Nair <95134716+DipinJayarajan@users.noreply.github.com>
2023-01-28 17:41:10 +0530

    topbar

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { useEffect } from 'react';
 import { useState } from 'react';
 import { BsChatSquareDots } from 'react-icons/bs'
 import {
@@ -14,13 +13,13 @@ const Topbar = () => {
 
     const [ nav, setNav ] = useState(false);
 
-    useEffect(() => {   
-
-    }, [])
+    const Toggle = () => {
+        setNav(!nav);
+    }
 
   return (
     <div className='w-full min-h-[50px] flex justify-between items-center absolute z-10 text-white bg-gray-700/80'>
-        <ul className='hidden md:flex'>
+        <ul className='hidden px-4 sm:flex'>
             <li>
                 <a href='/'>Home</a>
             </li>
@@ -34,20 +33,20 @@ const Topbar = () => {
                 <a href='#contact'>Contact</a>
             </li>
         </ul>
-        <div>
+        <div className='flex justify-between'>
             <FaFacebookF className='mx-4' />
             <FaTwitter className='mx-4'/>
             <FaGooglePlusG className='mx-4' />
             <FaInstagram className='mx-4' />
             </div>
-            <div>
+            <div className='text-xl md:hidden z-10 ' onClick={Toggle}>
             {/* hamburger icon */}
-            <FaBars className='text-xl' />
+            <FaBars className='mr-4 cursor-pointer' size={20} />
             </div>
-            <div>
-                {/* Mobile Menu */}
-                <ul>
-                <li>
+            {/* Mobile Menu */}
+            <div className={ nav ? "overflow-y-hidden md:hidden ease-in duration-300 absolute text-gray-300 left-0 top-0 h-screen bg-black opacity-90 w-full px-4 py-7 flex  justify-center items-center" : " absolute top-0 h-screen left-[-100%] ease-in duration-500" }>
+                <ul >
+                <li >
                 <a href='/'>Home</a>
             </li>
             <li>
